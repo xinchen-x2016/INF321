186421c0b008601b27eee250d0c00a5e235e2dce
Xin CHEN <32307644+xinchen-x2016@users.noreply.github.com>
2017-09-26 22:35:44 +0200

    Calculator

---
+++ GraphicButton.java
@@ -0,0 +1,6 @@
+package Calculator;
+import javax.swing.JButton;
+
+public class GraphicButton extends JButton{
+	
+}
---
+++ Instruction.java
@@ -0,0 +1,4 @@
+package Calculator;
+public enum Instruction {
+	PLUS, MINUS, MULT, DIV, CALC, NONE, CLEAR, OPEN, CLOSE, UMINUS, VAR;
+}
